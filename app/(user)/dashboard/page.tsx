8bc723e2771b55d9b0bc9c165e416691117f2b31
ppppppsmash <pppppp.work@gmail.com>
2024-09-25 19:43:51 +0900

    feat: Refactor dashboard page and create new project component

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,36 +1,9 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
+import NewProjBtn from "@/components/new-proj"
 
 export default function Page() {
   return (
-    <Dialog>
-      <DialogTrigger>Create Project</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>New Project</DialogTitle>
-          <DialogDescription>
-            Create a new project to get started.
-          </DialogDescription>
-        </DialogHeader>
-
-        <form>
-          <Label htmlFor="name">Name</Label>
-          <Input id="name" placeholder="Project Name" />
-          <Label htmlFor="url">URL</Label>
-          <Input id="url" placeholder="https://example.com" />
-          <Label htmlFor="description">Description</Label>
-          <Textarea id="description" placeholder="Project Description (optional)" />
-        </form>
-      </DialogContent>
-    </Dialog>
+    <div>
+      <NewProjBtn />
+    </div>
   )
 }
