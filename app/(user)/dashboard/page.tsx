3e6aef7c3e291ee94529aa8450592300f23886e1
ppppppsmash <pppppp.work@gmail.com>
2024-09-25 22:24:16 +0900

    feat: Add user ID to projects schema and implement user authentication in dashboard

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,6 +1,18 @@
+import { db } from "@/db";
+import { projects } from "@/db/schema";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
 import NewProjBtn from "@/components/new-proj"
 
-export default function Page() {
+export default async function Page() {
+  const allProjects = await db.select().from(projects);
+  
+  const { userId } = auth();
+  const user = await currentUser();
+
+  console.log(userId);
+  console.log(user);
+
   return (
     <div>
       <NewProjBtn />
