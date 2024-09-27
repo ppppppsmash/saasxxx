bfad187077cbddc857e9640a09c4b992eb24164f
ppppppsmash <pppppp.work@gmail.com>
2024-09-27 17:26:27 +0900

    feat: Enhance header menu with icons and links

--- components/header-menu.tsx
+++ components/header-menu.tsx
@@ -1,18 +1,17 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link';
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from '@/components/ui/button';
 
-import { Menu, X } from 'lucide-react';
+import { Menu, X, Folder, CreditCard } from 'lucide-react';
 
 const HeaderMenu = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -30,9 +29,25 @@ const HeaderMenu = () => {
           }
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem>Projects</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem asChild>
+          <Link
+            href="/dashboard"
+            className="flex items-center"
+          >
+            <Folder className="w-4 h-4 mr-2" />
+            <span>Projects</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link
+            href="/payments"
+            className="flex items-center"
+          >
+            <CreditCard className="w-4 h-4 mr-2" />
+            <span>Billing</span>
+          </Link>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
