4dc26bf3ed00ecaef990d26422dc2af0065f6fef
ppppppsmash <pppppp.work@gmail.com>
2024-11-11 22:06:15 +0900

    feat: update hero section layout and add responsive design

--- app/landing-page/hero.tsx
+++ app/landing-page/hero.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 import {
   SignUpButton,
   SignedIn,
@@ -11,33 +13,46 @@ import { Button } from '@/components/ui/button';
 const Hero = () => {
   return (
     <section className="grow">
-      <div className="container mx-auto px-4 my-24">
-        <div className="max-w-sm mb-8">
-          <h1 className="mb-5 text-5xl font-extrabold leading-tight">Collect your feedback seamlessly</h1>
-          <p className="text-gray-500 text-lg">Easily integrate Kurosawa and start collecting feedback today.</p>
+      <div className="container mx-auto px-4 mt-12 mb-24 flex flex-col md:flex-row">
+        <div className="flex flex-col max-w-sm justify-center">
+          <div className="mb-8">
+            <h1 className="mb-5 text-5xl font-extrabold leading-tight">Collect your feedback seamlessly</h1>
+            <p className="text-gray-500 text-lg">Easily integrate Kurosawa and start collecting feedback today.</p>
+          </div>
+          <div>
+            <SignedOut>
+              <SignUpButton>
+                <div className="flex gap-3">
+                  <Button>
+                    <LogIn className="w-4 h-4 mr-2" />
+                    Get Started
+                  </Button>
+                  <Button variant="secondary" asChild>
+                    <Link href="https://github.com">
+                      <Github className="w-4 h-4 mr-2" />
+                      GitHub
+                    </Link>
+                  </Button>
+                </div>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <Button asChild>
+                <Link href="/dashboard">Dashboard</Link>
+              </Button>
+            </SignedIn>
+          </div>
         </div>
-        <div>
-          <SignedOut>
-            <SignUpButton>
-              <div className="flex gap-3">
-                <Button>
-                  <LogIn className="w-4 h-4 mr-2" />
-                  Get Started
-                </Button>
-                <Button variant="secondary" asChild>
-                  <Link href="https://github.com">
-                    <Github className="w-4 h-4 mr-2" />
-                    GitHub
-                  </Link>
-                </Button>
-              </div>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <Button asChild>
-              <Link href="/dashboard">Dashboard</Link>
-            </Button>
-          </SignedIn>
+
+        <div className="flex-1 md:mt-2 mt-10">
+          <Image
+            src="/images/demo.gif"
+            alt="demo"
+            layout={"responsive"}
+            width={175}
+            height={175}
+            unoptimized={true}
+          />
         </div>
       </div>
     </section>
